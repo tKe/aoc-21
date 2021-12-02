2617c5da5c4ab0ba87d73b37a32e0c5082862167
Phil Davies <git@karn.co.uk>
2021-12-02 10:30:56 +0000

    tidy up Day01Test

--- kotlin/src/test/kotlin/year2021/Day01Test.kt
+++ kotlin/src/test/kotlin/year2021/Day01Test.kt
@@ -8,13 +8,13 @@ class Day01Test : FunSpec({
     with(Day01.testContext()) {
         context("part1") {
             test("example input produces result of 7") {
-                Day01["part1"]() shouldBe 7
+                puzzle["part1"]() shouldBe 7
             }
         }
 
         context("part2") {
             test("example input produces result of 5") {
-                Day01["part2"]() shouldBe 5
+                puzzle["part2"]() shouldBe 5
             }
         }
     }
