ce18b66d6add96e9215bbcca97b46e3d13f9e63a
Phil Davies <git@karn.co.uk>
2021-12-04 01:12:50 +0000

    refactor kotlin day2

--- kotlin/src/main/kotlin/year2021/Day02.kt
+++ kotlin/src/main/kotlin/year2021/Day02.kt
@@ -2,45 +2,42 @@ package year2021
 
 import Puzzle
 
+private data class Location(val horizontalPosition: Int = 0, val depth: Int = 0, val aim: Int = 0)
+private typealias Mover = Location.(Int) -> Location
+
 object Day02 : Puzzle2021(2, {
-    data class Location(val horizontalPosition: Int = 0, val depth: Int = 0, val aim: Int = 0)
-    fun Sequence<String>.parseCommands() =
-        map { it.split(' ', limit = 2) }
-            .map { (cmd, v) -> cmd to v.toInt() }
-    fun Sequence<Pair<String, Int>>.drive(engine: Location.(String, Int) -> Location) =
-        fold(Location()) { location, (command, amount) -> location.engine(command, amount) }
+    fun List<String>.drive(up: Mover, down: Mover, forward: Mover) = asSequence()
+        .map { it.split(' ', limit = 2) }
+        .map { (direction, amount) ->
+            when (direction) {
+                "up" -> up
+                "down" -> down
+                "forward" -> forward
+                else -> error("unhandled direction [$direction]")
+            } to amount.toInt()
+        }
+        .fold(Location()) { location, (move, amount) -> location.move(amount) }
+        .let { (a, b) -> a * b }
 
     solution("part1") {
-        withInput { lines ->
-            lines.parseCommands()
-                .drive { cmd, v ->
-                    when(cmd) {
-                        "up" -> copy(depth = depth - v)
-                        "down" -> copy(depth = depth + v)
-                        "forward" -> copy(horizontalPosition = horizontalPosition + v)
-                        else -> this
-                    }
-                }
-                .let { (a, b) -> a * b }
-        }
+        input.drive(
+            up = { copy(depth = depth - it) },
+            down = { copy(depth = depth + it) },
+            forward = { copy(horizontalPosition = horizontalPosition + it) }
+        )
     }
 
     solution("part2") {
-        withInput { lines ->
-            lines.parseCommands()
-                .drive { cmd, v ->
-                    when(cmd) {
-                        "up" -> copy(aim = aim - v)
-                        "down" -> copy(aim = aim + v)
-                        "forward" -> copy(
-                            horizontalPosition = horizontalPosition + v,
-                            depth = depth + (aim * v)
-                        )
-                        else -> this
-                    }
-                }
-                .let { (a, b) -> a * b }
-        }
+        input.drive(
+            up = { copy(aim = aim - it) },
+            down = { copy(aim = aim + it) },
+            forward = {
+                copy(
+                    horizontalPosition = horizontalPosition + it,
+                    depth = depth + (aim * it)
+                )
+            }
+        )
     }
 })
 
