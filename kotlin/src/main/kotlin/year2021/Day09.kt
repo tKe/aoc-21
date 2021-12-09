cf17a9f33d310f493442aa02ad5200e665044aa8
Phil Davies <git@karn.co.uk>
2021-12-09 18:13:08 +0000

    [kotlin] day-9 - return 10 if out of bounds (and don't filter neighbours)
    thanks to @dfings suggestion

--- kotlin/src/main/kotlin/year2021/Day09.kt
+++ kotlin/src/main/kotlin/year2021/Day09.kt
@@ -4,13 +4,12 @@ import Puzzle
 
 object Day09 : Puzzle2021(9, {
     data class Grid(val data: List<List<Int>>) {
-        operator fun get(point: Pair<Int, Int>) = point.let { (x, y) -> data[y][x] }
+        operator fun get(point: Pair<Int, Int>) = point.let { (x, y) -> if (x in cols && y in rows) data[y][x] else 10 }
         val rows = data.indices
         val cols = data.first().indices
         val indices = cols.asSequence().flatMap { rows.map(it::to) }
         fun neighbours(point: Pair<Int, Int>) =
             point.let { (x, y) -> sequenceOf((x - 1) to y, (x + 1) to y, x to (y - 1), x to (y + 1)) }
-                .filter { (x, y) -> x in cols && y in rows }
     }
 
     fun <R> List<String>.parseGrid(block: Grid.() -> R) = Grid(map { it.map(Char::digitToInt) }).block()
@@ -29,7 +28,7 @@ object Day09 : Puzzle2021(9, {
         input.parseGrid {
             indices.filter { this[it] < neighbours(it).minOf(::get) }
                 .map { min ->
-                    generateSequence(setOf(min) to listOf(min)) { (basin, candidates) ->
+                    generateSequence(emptySet<Pair<Int, Int>>() to listOf(min)) { (basin, candidates) ->
                         candidates.flatMap(::neighbours).distinct()
                             .filterNot(basin::contains).filter { this[it] < 9 }
                             .let { (basin + it) to it }
