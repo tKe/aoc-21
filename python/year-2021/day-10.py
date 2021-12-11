bdfbb6b5ebea62b3cccf3b9a48d96fa944e7f9f9
Phil Davies <git@karn.co.uk>
2021-12-11 21:50:19 +0000

    [python] unindent day-10

--- python/year-2021/day-10.py
+++ python/year-2021/day-10.py
@@ -8,33 +8,33 @@ with open('../../../aoc-21-inputs/year-2021/day-10/input.txt') as f:
     inputs = [x.strip() for x in f.readlines()]
 
 
-    def part1():
-        def score_input(line):
-            scores = {')': 3, ']': 57, '}': 1197, '>': 25137}
-            stack = deque()
-            for c in line:
-                if c in pairs:
-                    stack.appendleft(pairs[c])
-                elif c != stack.popleft():
-                    return scores[c]
+def part1():
+    def score_input(line):
+        scores = {')': 3, ']': 57, '}': 1197, '>': 25137}
+        stack = deque()
+        for c in line:
+            if c in pairs:
+                stack.appendleft(pairs[c])
+            elif c != stack.popleft():
+                return scores[c]
 
-        return sum(score for line in inputs if (score := score_input(line)))
-
-
-    def part2():
-        def score_input(line):
-            scores = {')': 1, ']': 2, '}': 3, '>': 4}
-            stack = deque()
-            for c in line:
-                if c in pairs:
-                    stack.appendleft(pairs[c])
-                elif c != stack.popleft():
-                    break
-            else:
-                return reduce(lambda acc, s: acc * 5 + scores[s], stack, 0)
-
-        return median(score for line in inputs if (score := score_input(line)))
+    return sum(score for line in inputs if (score := score_input(line)))
 
 
-    print(part1())
-    print(part2())
+def part2():
+    def score_input(line):
+        scores = {')': 1, ']': 2, '}': 3, '>': 4}
+        stack = deque()
+        for c in line:
+            if c in pairs:
+                stack.appendleft(pairs[c])
+            elif c != stack.popleft():
+                break
+        else:
+            return reduce(lambda acc, s: acc * 5 + scores[s], stack, 0)
+
+    return median(score for line in inputs if (score := score_input(line)))
+
+
+print(part1())
+print(part2())
