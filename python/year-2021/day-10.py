7ec063f726ebb2042538df22f1c913f9a4462098
Phil Davies <git@karn.co.uk>
2021-12-12 00:20:48 +0000

    [python,day-10] no need for deque

--- python/year-2021/day-10.py
+++ python/year-2021/day-10.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
-from collections import deque
 from functools import reduce
 from statistics import median
 
 pairs = {'(': ')', '{': '}', '[': ']', '<': '>'}
-with open('../../../aoc-21-inputs/year-2021/day-10/input.txt') as f:
+with open('../../../aoc-21-inputs/year-2021/day-10/example.txt') as f:
     inputs = [x.strip() for x in f.readlines()]
 
 
 def part1():
     def score_input(line):
         scores = {')': 3, ']': 57, '}': 1197, '>': 25137}
-        stack = deque()
+        stack = []
         for c in line:
             if c in pairs:
-                stack.appendleft(pairs[c])
-            elif c != stack.popleft():
+                stack.append(pairs[c])
+            elif c != stack.pop():
                 return scores[c]
 
     return sum(score for line in inputs if (score := score_input(line)))
@@ -24,11 +23,11 @@ def part1():
 def part2():
     def score_input(line):
         scores = {')': 1, ']': 2, '}': 3, '>': 4}
-        stack = deque()
+        stack = []
         for c in line:
             if c in pairs:
-                stack.appendleft(pairs[c])
-            elif c != stack.popleft():
+                stack.append(pairs[c])
+            elif c != stack.pop():
                 break
         else:
             return reduce(lambda acc, s: acc * 5 + scores[s], stack, 0)
