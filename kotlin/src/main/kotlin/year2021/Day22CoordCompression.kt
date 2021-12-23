c42fe2cc539d6b030dcdd410863e5d32d92ab3c8
Phil Davies <git@karn.co.uk>
2021-12-23 17:43:15 +0000

    [kotlin,day-22] improved coord compression solution

--- kotlin/src/main/kotlin/year2021/Day22CoordCompression.kt
+++ kotlin/src/main/kotlin/year2021/Day22CoordCompression.kt
@@ -3,77 +3,77 @@ package year2021
 import Puzzle
 import SolutionCtx
 import java.util.*
+import kotlin.math.max
+import kotlin.math.min
 
 fun main() = Puzzle.main(Day22CoordCompression)
 
 object Day22CoordCompression : Puzzle2021(22) {
 
     val part1 by solution {
-        parseInputs().countLit { it in -50..50 }
+        parseInputs(-50..50).countLit()
     }
 
     val part2 by solution {
         parseInputs().countLit()
     }
 
-    private fun List<Instruction>.countLit(filter: (Int) -> Boolean = { true }): Long {
-        val (xs, xLookup) = extractAxisLookup(Instruction::x, filter)
-        val (ys, yLookup) = extractAxisLookup(Instruction::y, filter)
-        val (zs, zLookup) = extractAxisLookup(Instruction::z, filter)
+    private fun List<Instruction>.countLit() =
+        CompressedBitSet3(extractAxis { x }, extractAxis { y }, extractAxis { z })
+            .also { forEach { (state, x, y, z) -> it[x, y, z] = state } }
+            .cardinality()
 
-        val grid = Array(xs.size) { Array(ys.size) { BooleanArray(zs.size) } }
-        grid.process(this, xLookup, yLookup, zLookup)
-        return grid.extract(xs, ys, zs)
-    }
+    class CompressedBitSet3(xValues: Iterable<Int>, yValues: Iterable<Int>, zValues: Iterable<Int>) {
+        private val xAxis = CompressedAxis(xValues)
+        private val yAxis = CompressedAxis(yValues)
+        private val zAxis = CompressedAxis(zValues)
+        private val height = yAxis.sizes.size
+        private val depth = zAxis.sizes.size
+        private val data = BitSet(xAxis.sizes.size * yAxis.sizes.size * zAxis.sizes.size)
 
-    private fun Array<Array<BooleanArray>>.extract(xs: List<Int>, ys: List<Int>, zs: List<Int>): Long {
-        val xr = xs.zipWithNext { a, b -> b - a }.withIndex()
-        val yr = ys.zipWithNext { a, b -> b - a }.withIndex().toList()
-        val zr = zs.zipWithNext { a, b -> b - a }.withIndex().toList()
-        return xr.sumOf { (x, xCnt) ->
-            yr.sumOf { (y, yCnt) ->
-                zr.sumOf { (z, zCnt) ->
-                    (if (this[x][y][z]) zCnt else 0L).toLong()
-                } * yCnt
-            } * xCnt
+        private fun idx(x: Int, y: Int, z: Int) = depth * height * x + y * depth + z
+        operator fun get(x: Int, y: Int, z: Int) = data[idx(xAxis[x], yAxis[y], zAxis[z])]
+        operator fun set(x: IntRange, y: IntRange, z: IntRange, value: Boolean) = with(zAxis[z]) {
+            for (xi in xAxis[x]) for (yi in yAxis[y]) data.set(idx(xi, yi, first), idx(xi, yi, last) + 1, value)
         }
-    }
 
-    private fun Array<Array<BooleanArray>>.process(
-        instructions: List<Instruction>, xMap: SortedMap<Int, Int>, yMap: SortedMap<Int, Int>,
-        zMap: SortedMap<Int, Int>
-    ) {
-        for (instruction in instructions) {
-            for (x in xMap[instruction.x]) {
-                for (y in yMap[instruction.y]) {
-                    for (z in zMap[instruction.z]) {
-                        this[x][y][z] = instruction.state
-                    }
+        fun cardinality(): Long {
+            var sum = 0L
+            var it = data.nextSetBit(0)
+            while (it >= 0) {
+                val x = it / (depth * height)
+                val y = it / depth % height
+                val z = it % (depth * height) % depth
+                sum += xAxis.sizes[x].toLong() * yAxis.sizes[y] * zAxis.sizes[z]
+                it = data.nextSetBit(it + 1)
+            }
+            return sum
+        }
+
+        private class CompressedAxis private constructor(val sizes: IntArray, private val lookup: Map<Int, Int>) {
+            operator fun get(r: Int) = lookup[r] ?: error("unknown raw value $r")
+            operator fun get(r: IntRange) = get(r.first) until get(r.last + 1)
+
+            companion object {
+                operator fun invoke(raw: Iterable<Int>) = raw.sorted().distinct().let {
+                    CompressedAxis(
+                        sizes = it.zipWithNext { a, b -> b - a }.toIntArray(),
+                        lookup = it.withIndex().associate { (idx, value) -> value to idx }
+                    )
                 }
             }
         }
     }
 
-    data class Instruction(val x: IntRange, val y: IntRange, val z: IntRange, val state: Boolean)
+    data class Instruction(val state: Boolean, val x: IntRange, val y: IntRange, val z: IntRange)
 
-    private inline fun List<Instruction>.extractAxisLookup(
-        crossinline extractor: Instruction.() -> IntRange,
-        filter: (Int) -> Boolean
-    ) = extractAxis(extractor, filter).let { it to it.createLookup() }
+    private inline fun List<Instruction>.extractAxis(crossinline axis: Instruction.() -> IntRange) =
+        flatMap { it.axis().run { sequenceOf(first, last + 1) } }
 
-    private inline fun List<Instruction>.extractAxis(
-        crossinline extractor: Instruction.() -> IntRange,
-        filter: (Int) -> Boolean
-    ) = flatMap { it.extractor().run { sequenceOf(first, last + 1) } }.filter(filter).distinct().sorted()
-
-    private fun List<Int>.createLookup() = mapIndexed { idx, value -> value to idx }.toMap().toSortedMap()
-
-    private operator fun SortedMap<Int, Int>.get(keyRange: IntRange) =
-        subMap(keyRange.first, keyRange.last + 1).values.asSequence()
-
-    private fun SolutionCtx.parseInputs(): List<Instruction> = input.map {
-        it.split("=", "..", ",").mapNotNull(String::toIntOrNull)
-            .chunked(2).map { (a, b) -> a..b }
-            .let { (x, y, z) -> Instruction(x, y, z, it.startsWith("on")) }
-    }
+    private fun SolutionCtx.parseInputs(range: IntRange = Int.MIN_VALUE..Int.MAX_VALUE): List<Instruction> =
+        input.map { line ->
+            line.split("=", "..", ",").mapNotNull(String::toIntOrNull)
+                .chunked(2).map { (a, b) -> max(a, range.first)..min(b, range.last) }
+                .let { (x, y, z) -> Instruction(line.startsWith("on"), x, y, z) }
+        }.filterNot { it.x.isEmpty() || it.y.isEmpty() || it.z.isEmpty() }
 }
