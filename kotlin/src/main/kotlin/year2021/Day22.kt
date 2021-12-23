e658bd481c9d1c8c29a39b5f6d58b0a0612313c4
Phil Davies <git@karn.co.uk>
2021-12-23 00:25:44 +0000

    [kotlin,day-22] tidy up code a little

--- kotlin/src/main/kotlin/year2021/Day22.kt
+++ kotlin/src/main/kotlin/year2021/Day22.kt
@@ -10,37 +10,31 @@ fun main() = Puzzle.main(Day22)
 object Day22 : Puzzle2021(22) {
 
     val part1 by solution {
-        parseRebootSteps().filterTo(Cuboid(-50..50)).countFinalOn()
+        parseRebootSteps().filterTo(Cuboid(-50..50)).countLit()
     }
 
     val part2 by solution {
-        parseRebootSteps().countFinalOn()
+        parseRebootSteps().countLit()
     }
 
-    private fun List<RebootStep>.countFinalOn(cache: MutableMap<Any, Long> = mutableMapOf()) =
-        mapWithSubsequent { step, subsequent ->
+    private fun List<RebootStep>.countLit(cache: MutableMap<Any, Long> = mutableMapOf()) = this
+        .mapWithSubsequent { step, subsequent ->
             step to subsequent.filterTo(step.cuboid).map(RebootStep::cuboid)
-        }.filter { (step) -> step.state }.sumOf { (step, overlaps) -> step.cuboid.sizeExcluding(overlaps, cache) }
+        }
+        .filter { (step) -> step.state }
+        .sumOf { (step, overlaps) -> step.cuboid.sizeExcluding(overlaps, cache) }
 
     private data class Cuboid(val xs: IntRange, val ys: IntRange, val zs: IntRange = 0..0) {
         constructor(cubicRange: IntRange) : this(cubicRange, cubicRange, cubicRange)
 
-        val size = 1L * xs.size * ys.size * zs.size
-        fun isEmpty() = xs.isEmpty() || ys.isEmpty() || zs.isEmpty()
+        val size = xs.size * ys.size * zs.size
+        val isEmpty = size == 0L
         fun intersect(other: Cuboid) = Cuboid(
             xs.intersect(other.xs),
             ys.intersect(other.ys),
             zs.intersect(other.zs)
         )
     }
-
-    private fun IntRange.intersect(other: IntRange) = max(first, other.first)..min(last, other.last)
-    private val IntRange.size get() = if (isEmpty()) 0 else 1 + last - first
-
-    private data class RebootStep(val cuboid: Cuboid, val state: Boolean)
-
-    private fun List<RebootStep>.filterTo(cuboid: Cuboid) =
-        map { it.copy(cuboid = cuboid.intersect(it.cuboid)) }.filterNot { it.cuboid.isEmpty() }
 
     private fun Cuboid.sizeExcluding(overlaps: List<Cuboid>, cache: MutableMap<Any, Long>): Long =
         cache.getOrPut(this to overlaps) {
@@ -49,12 +43,20 @@ object Day22 : Puzzle2021(22) {
             }.sum()
         }
 
-    private fun <T, R> List<T>.mapWithSubsequent(transform: (element: T, subsequent: List<T>) -> R) =
-        mapIndexed { index: Int, element: T -> transform(element, subList(index + 1, size)) }
+    private data class RebootStep(val cuboid: Cuboid, val state: Boolean)
 
     private fun SolutionCtx.parseRebootSteps() = input.map {
         it.split("=", "..", ",").mapNotNull(String::toIntOrNull)
             .chunked(2) { (a, b) -> a..b }
             .let { (x, y, z) -> RebootStep(Cuboid(x, y, z), it.startsWith("on")) }
     }
+
+    private fun List<RebootStep>.filterTo(cuboid: Cuboid) =
+        map { it.copy(cuboid = cuboid.intersect(it.cuboid)) }.filterNot { it.cuboid.isEmpty }
+
+    private fun IntRange.intersect(other: IntRange) = max(first, other.first)..min(last, other.last)
+    private val IntRange.size get() = if (isEmpty()) 0 else 1L + last - first
+
+    private inline fun <T, R> List<T>.mapWithSubsequent(transform: (element: T, subsequent: List<T>) -> R) =
+        mapIndexed { index: Int, element: T -> transform(element, subList(index + 1, size)) }
 }
